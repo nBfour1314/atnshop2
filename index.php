f323b248e8c9dbcc2bb43470b64892bde53452af
nBfour1314 <nguyennhathao11c2@gmail.com>
2021-11-11 08:11:37 +0700

    1st2

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
   ?>
   <nav class="navbar navbar-expand-lg navbar-light bg-light ">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><img src="./tree/img/logo1.png" alt="logo" class="logo">ATN</a>
+      <a class="navbar-brand" href="index.php">ATN SHOP</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
