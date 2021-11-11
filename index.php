da30a245163e7d14ba2421ea94af0ac42f3dd3fc
nBfour1314 <nguyennhathao11c2@gmail.com>
2021-11-11 08:07:31 +0700

    1st2

--- index.php
+++ index.php
@@ -2,42 +2,27 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  <!-- Bootstrap-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <!-- CSS-->
   <link rel="stylesheet" href="./tree/css/Style.css">
-
-  <!-- fontAwsomae-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-  <!-- webicon -->
-  <link rel="shortcut icon" href="./tree/img/logo1.png">
-
-  <!-- Custom CSS -->
+  <link rel="shortcut icon" href="">
   <link rel="stylesheet" href="./tree/css/owl.carousel.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="./tree/css/responsive.css">
-
-  <!--datatable-->
   <script src="js/jquery-3.2.0.min.js"></script>
   <script src="js/jquery.dataTables.min.js"></script>
   <script src="js/dataTables.bootstrap.min.js"></script>
-
-
   <title>ATN Shop</title>
 </head>
 
 <body>
-  <!-- Optional JavaScript; choose one of the two! -->
-  <!-- Option 1: Bootstrap Bundle with Popper -->
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
@@ -57,9 +42,6 @@
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#footers">About us</a>
-          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Management
@@ -67,38 +49,10 @@
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="?page=category_management">Category</a></li>
               <li><a class="dropdown-item" href="?page=product_management">Product</a></li>
-              <li><a class="dropdown-item" href="?page=shops_management">Shops</a></li>
-            </ul>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Product
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#sessionproduct">Latest Products</a></li>
-              <li><a class="dropdown-item" href="#sessionproduct">Best Sellers</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="?page=shop">See more</a></li>
+              <li><a class="dropdown-item" href="?page=shops_management">Store</a></li>
             </ul>
           </li>
         </ul>
-        <!-- <ul class="nav-item">
-                <form class="navbar-form navbar-left" action="index.php?page=search" method="post">
-                        <div class="form-group">
-                      
-                        </div>                        
-                </form>
-        </ul> -->
-        
-        <!-- <form class="d-flex">
-          <ul class="nav navbar-nav navbar-right">
-            <li><button><a href="?page=cart" style="font-size:24px">
-                <i class="fa fa-shopping-cart"></i>Cart</a></button></li>
-          </ul>
-        </form> -->
         <form class="d-flex" action="index.php?page=search" method="post">
           <input class="form-control me-2" type="text" name="txtSearch" placeholder="Search" aria-label="Search">
           <button type="submit" class="btn btn-info">Search</button>
@@ -114,17 +68,12 @@
               <i class="fas fa-user-edit">Hi, <?php echo $_SESSION['us']?></i>
               </a>
               </li>
-              <li><a href="?page=logout" style="color: #000;">
-              <i class="fas fa-sign-out-alt">Logout</i>
-              </a>
-              </li>
+              <li><a href="?page=logout" style="color: #000;">Logout</a></li>
             <?php
             } else {
             ?>
-              <li><a href="?page=login" style="color:#000">
-                  <i class="fa fa-lock"></i>Login</a></li>
-              <li><a href="?page=register" style="color:#000">
-                  <i class="fa fa-star"></i>Register</a></li>
+              <li><a href="?page=login" style="color:#000">Login</a></li>
+              <li><a href="?page=register" style="color:#000">Register</a></li>
             <?php
             }
             ?>
@@ -134,9 +83,6 @@
     </div>
   </nav>
 </div>
-
-
-  <!-- End Menu -->
 
   <?php
   if (isset($_GET['page'])) {
@@ -197,9 +143,9 @@
         <div class="col span-1-of-3">
           <ul class="information">
             <h2>Address</h2>
-            <li><i class="fas fa-map-marker-alt"></i>Address: 21, Nguyen Trung Truc street, Ninh Kieu District, Can Tho City</li>
-            <li><i class="fas fa-envelope"></i>Email: duonghqgcc19037@fpt.edu.vn</li>
-            <li><i class="fas fa-phone"></i>Phone: (+84)333119697</li>
+            <li><i class="fas fa-map-marker-alt"></i>Address: 160, 30/4 street, Ninh Kieu District, Can Tho City</li>
+            <li><i class="fas fa-envelope"></i>Email: haonngcc19037@fpt.edu.vn</li>
+            <li><i class="fas fa-phone"></i>Phone: (+84)0399989631</li>
           </ul>
           <ul class="social-icons">
             <li><a href="https://www.facebook.com/duongquoccute"><i class="fab fa-facebook"></i></a></li>
@@ -212,7 +158,7 @@
           <div class="social-icons">
             <div class="information">
               <h2>About Us</h2>
-              <p>Dery is a stationery store that supports and sells stationery, consumer electronics, and services.
+              <p>ATN SSHOP is the most choice in VietNam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id dolor cum rerum amet, exercitationem qui? Voluptas libero aut reprehenderit ullam.
               </p>
             </div>
           </div>
@@ -223,7 +169,6 @@
               <h2>Menu</h2>
               <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
                 <li><a href="#">Product</a></li>
               </ul>
             </div>
@@ -232,7 +177,7 @@
       </div>
     </section>
     <p class="copyright">
-      Copyright &copy; 2021 by Dery Team
+      Copyright 2021&copy; By ATN SHOP
     </p>
   </footer>
 
