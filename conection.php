6c9bc7d79d3711205d18daf070b9f190ee430b3b
nBfour1314 <91709361+nBfour1314@users.noreply.github.com>
2021-11-11 08:21:05 +0700

    Update conection.php

--- conection.php
+++ conection.php
@@ -1,4 +1,6 @@
 <?php
-$conn = pg_connect("postgres://gdepmunemtksgf:8e5dcc60a520192f49a29ed114be58f6795b37ad2c962d9c5bee0feb9eb0885b@ec2-44-197-136-81.compute-1.amazonaws.com:5432/d4rlcsem44akip")
+/*$conn = pg_connect("postgres://gdepmunemtksgf:8e5dcc60a520192f49a29ed114be58f6795b37ad2c962d9c5bee0feb9eb0885b@ec2-44-197-136-81.compute-1.amazonaws.com:5432/d4rlcsem44akip")
+or die ("can not connect database".pg_connect_error());*/
+$conn = pg_connect("postgres://xlahdxpxsbiuza:aabbdae13b6d79fde27af02a36141e3228f20b230ab591a8a8b016846c24fd52@ec2-34-224-239-147.compute-1.amazonaws.com:5432/d3mbu19sk3rper")
 or die ("can not connect database".pg_connect_error());
 ?>
